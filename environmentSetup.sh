ec61f49cd23fda05ea3eb6e2d99b8cc9f2191697
joskim <joskim@csumb.edu>
2016-03-06 14:27:40 -0800

    Update environmentSetup.sh

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -8,12 +8,12 @@ function getAndroidSDK(){
 
   if [ ! -e $DEPS ]; then
     cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-    echo y | android update sdk -u -a -t android-19 &&
+    echo y | android update sdk -u -a -t android-23 &&
     echo y | android update sdk -u -a -t platform-tools &&
-    echo y | android update sdk -u -a -t build-tools-21.1.2 &&
-    echo y | android update sdk -u -a -t sys-img-x86-android-19 &&
+    echo y | android update sdk -u -a -t build-tools-23.0.2 &&
+    echo y | android update sdk -u -a -t sys-img-x86-android-23 &&
     #echo y | android update sdk -u -a -t addon-google_apis-google-18 &&
-    echo no | android create avd -n testAVD -f -t android-19 --abi default/x86 &&
+    echo no | android create avd -n testAVD -f -t android-23 --abi default/x86 &&
     touch $DEPS
   fi
 }
